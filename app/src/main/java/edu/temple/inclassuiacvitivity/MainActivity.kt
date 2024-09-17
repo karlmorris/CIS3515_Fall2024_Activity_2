974f685f4c5af217be7a4db41081559945eb1153
mariam0ashraf <mariam00ashraf@gmail.com>
2024-09-17 15:19:54 -0400

    2, 3, 4 steps done

--- app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
@@ -2,6 +2,7 @@ package edu.temple.inclassuiacvitivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.*
 class MainActivity : AppCompatActivity() {
@@ -13,14 +14,37 @@ class MainActivity : AppCompatActivity() {
         val displayTextView = findViewById<TextView>(R.id.textDisplay)
 
         /* TODO Step 2: Populate this array with multiples of 5 from 5 - 100*/
-        //val numberArray = Array Of Numbers
+        val numberArray = IntArray(20) { i -> (i + 1) * 5 }.toList()
+        Log.d("array output",numberArray.toString())
 
         /* TODO Step 3: Create adapter to display items from array in Spinner */
         //spinner.adapter = ArrayAdapter...
+        val numberList = numberArray.map { it.toString() }
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, numberList)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
 
 
         // TODO Step 4: Change TextView's text size to the number selected in the Spinner */
         //spinner.onItemSelectedListener = object: ...
+        // TODO Step 4: Change TextView's text size to the number selected in the Spinner
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                // Get the selected number
+                val selectedNumber = numberArray[position]
+
+                // Set the TextView's text to display the selected number
+                displayTextView.text = "Selected: $selectedNumber"
+
+                // Change the TextView's text size based on the selected number
+                displayTextView.textSize = selectedNumber.toFloat() // Set text size
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                displayTextView.text = "Please select a number."
+            }
+        }
+
 
     }
 }
